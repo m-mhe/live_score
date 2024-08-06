6be22cb359281720f93b11e3635f2cd2a09eb511
Momin Hosan Emon <mominhasanemon@gmail.com>
2024-08-06 23:09:44 +0600

    firebaseFirestore added

--- lib/app.dart
+++ lib/app.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       title: 'Live Score',
       home: const HomeScreen(),
       theme: ThemeData(
-          textTheme: TextTheme(
+          textTheme: const TextTheme(
               titleLarge: TextStyle(
                   color: Colors.black,
                   fontWeight: FontWeight.w700),
