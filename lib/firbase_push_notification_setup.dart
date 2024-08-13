88c8cdf0d80e4aa1cd0cad229048c1541c0d2d26
Momin Hosan Emon <mominhasanemon@gmail.com>
2024-08-13 18:11:24 +0600

    Firebase push notification setup

---
+++ lib/firbase_push_notification_setup.dart
@@ -0,0 +1,59 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+
+class FirBasePushNotificationSetup{
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  FirBasePushNotificationSetup._();
+  static final FirBasePushNotificationSetup instance = FirBasePushNotificationSetup._();
+  Future<void> initialize() async{
+    await _firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    //Foreground
+    FirebaseMessaging.onMessage.listen((RemoteMessage message){
+      if (kDebugMode) {
+        print(message.notification?.title);
+      }
+      if (kDebugMode) {
+        print(message.notification?.body);
+      }
+      if (kDebugMode) {
+        print(message.data);
+      }
+    });
+
+    //Background
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message){
+      debugPrint(message.notification?.title);
+      debugPrint(message.notification?.body);
+      debugPrint(message.data.toString());
+    });
+
+    //Terminated
+    FirebaseMessaging.onBackgroundMessage(justDemo);
+
+    await _getToken();
+    await _onTokenRefresh();
+  }
+
+  Future<void> _getToken() async{
+    String? token = await _firebaseMessaging.getToken();
+    print("Token :  $token");
+  }
+  Future<void> _onTokenRefresh() async{
+    _firebaseMessaging.onTokenRefresh.listen((String token){
+      print("Token :  $token");
+    });
+  }
+}
+
+Future<void> justDemo(RemoteMessage message) async{
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
+import 'firbase_push_notification_setup.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async{
@@ -8,5 +9,6 @@ Future<void> main() async{
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirBasePushNotificationSetup.instance.initialize();
   runApp(const MyApp());
 }
